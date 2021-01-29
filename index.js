ec29a913b8be4e9999d032a3ae54ec4b02079356
Nomentsafidy <mnomentsafidy@gmail.com>
2021-01-29 16:03:08 +0300

    feat maxmin function

--- index.js
+++ index.js
@@ -10,6 +10,7 @@
 
 
 let sommeBtn = document.querySelector("#somme");
+let maxminBtn = document.querySelector("#maxmin");
 
 /**
  * function somme de n premiere entier
@@ -22,3 +23,43 @@ sommeBtn.addEventListener("click", ()=>{
     }
     alert(`la somme des ${n} premiers nombres est : ${s}`)
 }, false);
+
+
+// Algorithme MaxMin;
+// Var I,N,Max,Min,X :entier ;
+// Début
+// Ecrire(‘Donner un entier N>0’) ;
+// Répéter Lire(N) ; Jusqu’à N>0 ;
+/* Lire le premier élément, puis initialiser le Min et le Max a cette valeur Lire(X) ; Max—X ; Min—X ;
+Pour I —2 a N Faire
+/* lire la suite des éléments et mettre a jour le Min et le Max
+Lire(X) ;
+Si Max<X Alors Max—X
+Sinon Si Min>X Alors Min—X Fsi
+Fsi ;
+Fait ;
+Ecrire(‘Le Minimun des valeurs est: ’,Min,’ le Maximum est : ‘,Max) ;
+Fin
+
+
+/**
+ * function maxmin
+ */
+maxminBtn.addEventListener("click", ()=>{
+    let min,max, n, x; 
+    n = parseInt(prompt("Donner un entier N>0"));
+    x = parseInt(prompt("Entrer le premier élément"));
+    max = x;
+    min = x;
+    for(let i = 2; i <= n; i++){
+        x = parseInt(prompt("Entrer l'élement"+ i +" ."));
+        if (x < min) {
+            min = x
+        } else if(x > max) {
+            max = x
+        }
+    }
+    alert(`Le Minimun des valeurs est: ${min} le Maximum est : ${max}`);
+}, false);
+
+
